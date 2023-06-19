b668ce6e506b50d8e984ec27009529efa6b2c408
jongjin1010 <jongjin024@gmail.com>
2023-06-19 11:45:29 +0900

    solve: [같은 숫자는 싫어]

---
+++ Level1/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+  var answer = [];
+
+  arr.forEach((element, index) => {
+    if (arr[index] !== arr[index + 1]) {
+      answer.push(element);
+    }
+  });
+
+  return answer;
+}
